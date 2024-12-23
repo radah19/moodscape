65d7552716bd3e39cf5615f5f64dba0af994529c
Anuj Adulla <anuj.adulla@gmail.com>
2024-12-23 01:12:36 -0500

    Spinner Working Now

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -87,15 +87,17 @@ const LoginPage = ({user, setUser, rerouteIfLoggedIn}) => {
                 <div className="flex items-center justify-between">
 
                 {/* Sign in Button */}
-                {   loading ? 
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled" type="button" >
-                        <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"/>
-                    </button> :             
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={authenticateCredentials}>
-                        Sign In
-                    </button>
-                }
-
+                <button className="flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={authenticateCredentials}>
+                    Sign In
+                    {
+                        loading ? 
+                        <svg className="animate-spin ml-2 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg> : <></>
+                    }
+                </button>
+                
                 {/* Forgot Password */}
                 <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
                     Forgot Password?
