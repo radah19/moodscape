0f15c0ab857e6219189fb757158e8e8f192ef062
Anuj Adulla <anuj.adulla@gmail.com>
2025-01-20 15:38:10 -0500

    Oops!

--- client/src/pages/RoomPage.jsx
+++ client/src/pages/RoomPage.jsx
@@ -97,8 +97,8 @@ const RoomPage = (props) => {
 
                 {/* Button to go back */}
                 <button onClick={() => {navigate('/')}} id="customize_room">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
                     </svg>
                 </button>
 
