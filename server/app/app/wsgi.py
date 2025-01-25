7db609d7f118b7d895e8119cdfe60c6eea1b1e04
Anuj Adulla <anuj.adulla@gmail.com>
2025-01-25 17:07:21 -0500

    Adding more fuelto my commit history

--- server/app/app/wsgi.py
+++ server/app/app/wsgi.py
@@ -12,7 +12,8 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append('/app')
+sys.path.append('/app/app')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.app.settings')
 
