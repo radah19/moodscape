bd5f83757915148c245cf6554e53aa56be6edbba
Anuj Adulla <anuj.adulla@gmail.com>
2025-01-25 19:17:18 -0500

    Oops

--- server/app/app/wsgi.py
+++ server/app/app/wsgi.py
@@ -15,6 +15,6 @@ from django.core.wsgi import get_wsgi_application
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.app.settings')
 
 application = get_wsgi_application()
