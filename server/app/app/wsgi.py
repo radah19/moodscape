7d39c563c5ba7f8237c85fb2627489b4bf550fad
Anuj Adulla <anuj.adulla@gmail.com>
2025-01-25 19:31:37 -0500

    Changes to allowed hosts for debugging

--- server/app/app/wsgi.py
+++ server/app/app/wsgi.py
@@ -14,6 +14,7 @@ from django.core.wsgi import get_wsgi_application
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, 'app'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.app.settings')
 
