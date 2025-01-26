0e9bafb5e5d904b32fe1c63384c30e28513fb37a
Anuj Adulla <anuj.adulla@gmail.com>
2025-01-25 19:42:20 -0500

    Sad face

--- server/app/app/wsgi.py
+++ server/app/app/wsgi.py
@@ -12,9 +12,8 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-sys.path.append(os.path.join(BASE_DIR, 'app'))
+sys.path.append('/app')
+sys.path.append('/app/app')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.app.settings')
 
